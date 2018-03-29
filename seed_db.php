4375499ba12c1331aaf69b5e0b37ca018399e3f7
Ioannis Botis <iobotis4@gmail.com>
2018-03-29 19:32:09 +0300

    Update seed_db.php

--- seed_db.php
+++ seed_db.php
@@ -4,6 +4,7 @@ $total = intval($argv[1]);
 $minPrice = intval($argv[2]);
 $maxPrice = intval($argv[3]);
 
-$stmt = $pdo->prepare("SET @p0='1000'; SET @p1='0'; SET @p2='1000'; CALL `InsertRand`(@p0, @p1, @p2);");
-$stmt->execute([$id]);
+$stmt = $pdo->prepare("SET @p0=?; SET @p1=?; SET @p2=?; CALL `InsertRand`(@p0, @p1, @p2);");
+$stmt->execute([$total,$minPrice,$maxPrice]);
 $addedProductsTotal = $stmt->rowCount();
+echo "Successfully added " . $addedProductsTotal . " products, with price range " . $minPrice . "-" . $maxPrice;
