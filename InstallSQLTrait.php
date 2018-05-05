1128f4051303b896a7d0592adcbcb1fe65db4ea8
iobotis <iobotis4@gmail.com>
2018-05-05 22:55:38 +0300

    Install trait used.

--- InstallSQLTrait.php
+++ InstallSQLTrait.php
@@ -2,4 +2,30 @@
 
 trait InstallSQLTrait {
 
+    private $pdo;
+
+    protected function setPdo(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function install()
+    {
+        if(!$this->isInstalled()) {
+            $sql = file_get_contents('install.sql');
+            $this->pdo->exec($sql);
+        }
+    }
+
+    public function isInstalled()
+    {
+        try {
+            $result = $this->pdo->query("SELECT 1 FROM products LIMIT 1");
+            return true;
+        } catch (Exception $e) {
+            // We got an exception == table not found
+            return false;
+        }
+        return false;
+    }
 }
--- KnapsackAlgorithm.php
+++ KnapsackAlgorithm.php
@@ -1,6 +1,9 @@
 <?php
+require_once('InstallSQLTrait.php');
 
 class KnapsackAlgorithm {
+
+    use InstallSQLTrait;
 
     private $pdo;
     private $table = 'products';
@@ -8,27 +11,7 @@ class KnapsackAlgorithm {
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
-    }
-    
-    public function install()
-    {
-        if(!$this->isInstalled()) {
-            $sql = file_get_contents('install.sql');
-            $this->pdo->exec($sql);
-        }
-    }
-    
-    public function isInstalled()
-    {
-        $table = $this->table;
-        try {
-            $result = $this->pdo->query("SELECT 1 FROM $table LIMIT 1");
-            return true;
-        } catch (Exception $e) {
-            // We got an exception == table not found
-            return false;
-        }
-        return false;
+        $this->setPdo($pdo);
     }
     
     public function findOneCollection()
