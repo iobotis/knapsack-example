298ea821b61f9dfcdc3b786a1a2e5e7cbece36ab
Ioannis Botis <iobotis4@gmail.com>
2018-05-12 00:13:41 +0300

    Create SaveResultTrait.php

---
+++ SaveResultTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+trait SaveResultTrait {
+
+    private $pdo;
+    protected function setPdo(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    
+    protected function insertToResults($collection, $totalItems, $price)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO results(totalItems, totalPrice, items)
+             VALUES(:totalItems, :totalPrice, :items)");
+        $statement->execute(array(
+            "totalItems" => $totalItems,
+            "totalPrice" => $price,
+            "items" => $collection
+        ));
+    }
+}
