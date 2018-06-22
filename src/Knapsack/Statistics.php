b8590ae97a288c76de4f14a2cf5a2b90fc2990ec
iobotis <iobotis4@gmail.com>
2018-06-23 02:00:04 +0300

    Statistics class created.2

--- src/Knapsack/Statistics.php
+++ src/Knapsack/Statistics.php
@@ -19,6 +19,12 @@ class Statistics
 
     private $error;
 
+    public function __construct(\PDO $pdo, Config $config)
+    {
+        $this->setPdo($pdo);
+        $this->setConfig($config);
+    }
+
     protected function setPdo(\PDO $pdo)
     {
         $this->pdo = $pdo;
@@ -39,7 +45,10 @@ class Statistics
     public function getTotalItemsInRange($min, $max)
     {
         // @todo add where clause.
-        $result = $this->pdo->query("SELECT count(*) FROM " . $this->config->tableName);
+        $result = $this->pdo->query(
+            "SELECT count(*) FROM " . $this->config->tableName
+            . " WHERE price >= '$min' AND price < '$max'"
+        );
         $result->execute();
         return $result->fetchColumn();
     }
